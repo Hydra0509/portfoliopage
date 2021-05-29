06a8d3813b8bfcb08c87cb2a8e0c5e818c2e2835
Kovács Attila <giga05091@hotmail.com>
2021-05-29 15:38:35 +0200

    idk anymore

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ return (
 	<Link className="orot" to="/portfoliopage">Orovecz Tamás</Link>
 	<header className="headertop">
 	<Link className="headerbuttons" to="/portfoliopage">Home</Link>
-	<Link className="headerbuttons" to="/about">About</Link>
+	<Link className="headerbuttons" to="/about">About me</Link>
 	<a className="headerbuttons" target="_blank" href="https://www.instagram.com/thomm.design/">Instagram</a>
 	</header>
 	</>
